f0d875d60e0d07b9e5c1378695a78cfb0bbf0ead
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-15 22:32:16 +0530

    add: snakes to fall down in the board

---
+++ Snakes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Snake_And_Ladder
+{
+    class Snakes
+    {
+        private static readonly int[,] SNAKES = new int[7, 2] { { 32, 10 }, { 36, 6 }, { 48, 26 }, { 62, 18 }, { 88, 24 }, { 95, 56 }, { 97, 78 } };
+        public static void PotentailOffalling(Players player)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                if (SNAKES[i, 0] == player.getCurrentPlace())
+                {
+                    player.setCurrentPlace(SNAKES[i, 1]);
+                }
+            }
+        }
+    }
+}
