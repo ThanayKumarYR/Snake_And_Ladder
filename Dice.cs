3077f8b896d667400a8c0b9af7c3c81e32fd507f
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-15 18:42:07 +0530

    adding: Now users can roll dice

---
+++ Dice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Snake_And_Ladder
+{
+    class Dice
+    {
+        public static void rollDice(Players[] obj)
+        {
+            Random random = new Random();   
+            foreach (var item in obj)
+            {
+                int place = item.getCurrentPlace();
+                item.setCurrentPlace(place + random.Next(1,7));
+                item.printPlace();
+            }
+            Console.WriteLine();
+        }
+    }
+}
