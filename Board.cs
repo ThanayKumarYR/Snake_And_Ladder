b902c776ce4506bc1d2efd8a42f7d9013a903668
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-15 19:55:26 +0530

    add: Board rules and diplay winner

---
+++ Board.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_And_Ladder
+{
+    class Board
+    {
+        public static int rules(Players players, int diceNumber)
+        { 
+            if ((players.getCurrentPlace()+diceNumber)<100)
+                return 200;
+            else if ((players.getCurrentPlace() + diceNumber) == 100)
+                return players.getId();
+            else return 404;
+        }
+
+    }
+}
