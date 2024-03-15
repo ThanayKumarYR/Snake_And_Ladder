a1eb94a53524f9d7a86cff8fd18eaa2ce15decf0
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-15 18:07:46 +0530

    adding: can create 2 to 10 players via this class !

---
+++ Players.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_And_Ladder
+{
+    class Players
+    {
+        private string name;
+        private int id;
+        private int currentPlace;
+        public Players(int userid, string userName)
+        {
+            id = userid;
+            name = userName;
+            currentPlace = 0;
+        }
+
+        public int getId() { return id; }
+        public int getCurrentPlace() { return currentPlace;}
+        public string getName() { return name; }
+
+        public void  setCurrentPlace(int place)
+        { currentPlace = place; }
+
+        public void status()
+        { 
+            int playerId = getId();
+            string  playerName = getName();
+            int playerPlace = getCurrentPlace();
+            Console.WriteLine($"Player Id = {playerId}, Player Name = {playerName}, Current Place = {playerPlace}");
+        }
+
+        public void printPlace()
+        {
+            Console.Write($"\t{name} @ {currentPlace}");
+        }
+    }
+}
